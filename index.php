de26316f5a1c423bebb30a4b1fda60c4482fe465
jeremy <jeremy.nowak@laplateforme.io>
2023-09-18 11:57:18 +0200

    Github and Altorouter setup

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form>
-        <input type="text" name="num1" placeholder="Number 1">
-        <input type="text" name="num2" placeholder="Number 2">
-    </form>
-</body>
-</html>
+<?php
+require 'vendor/autoload.php';
+$router = new AltoRouter();
+
+$router->setBasePath('/super-reminder/');
+
+$router->map( 'GET', '/', function(){
+    require_once "src/View/home.php";
+});
+?>
