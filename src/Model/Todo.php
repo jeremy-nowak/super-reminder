1500fafda9e41214c20bc9a375d6e75fade8a757
alixunderscore <alix.platier-favier@laplateforme.io>
2023-09-22 10:50:28 +0200

    sortTask function

--- src/Model/Todo.php
+++ src/Model/Todo.php
@@ -72,6 +72,18 @@ public function registerTask(){
     
 }
 
+// public function sortTask($listName){
+//     $idList = $this->getIdList($listName)['id_list'];
+
+//     $stmt = "SELECT * FROM task WHERE id_list = :id_list ORDER BY priority ASC";
+//     $stmt = $this->bdd->prepare($stmt);
+//     $stmt->execute(array(
+//         'id_list' => $idList
+//     ));
+//     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     return $result;
+// }
+
 // public function registerListBdd($listName, $task, $priority){
 //     $sql = "INSERT INTO `task` (`list_name`, `task`, `priority`, `start_date`) VALUES (:list_name, :task, :priority, NOW())";
 //     $prepare = $this->bdd->prepare($sql);
@@ -94,6 +106,7 @@ public function addTodo($id, $list, $title, $description, $start_date, $end_date
 
 
 }
+
 
 
 
